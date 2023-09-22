b560390993df9c89ee24f5cbc18dc1b0698c3e45
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-09-22 21:42:42 +0530

    animating feature

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 let cursor = new MouseFollower();
+
+gsap.to(".left_elem", {
+  scrollTrigger: {
+    trigger: "#featured_images", 
+    pin: true,
+    start: "top top", 
+    end: "bottom bottom", 
+    endTrigger: ".last",
+    scrub: 1, 
+  },
+  y: "-300%", 
+  ease: Power1, 
+});
+
