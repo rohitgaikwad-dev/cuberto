70eeb32e0ac579fe78b1e620016054e76fbdb3c2
Rohit Gaikwad <Rohitgaikwad78963@gmail.com>
2023-09-23 21:30:36 +0530

    feature animation

--- js/script.js
+++ js/script.js
@@ -2,14 +2,28 @@ let cursor = new MouseFollower();
 
 gsap.to(".left_elem", {
   scrollTrigger: {
-    trigger: "#featured_images", 
+    trigger: "#featured_images",
     pin: true,
-    start: "top top", 
-    end: "bottom bottom", 
+    start: "top top",
+    end: "bottom bottom",
     endTrigger: ".last",
-    scrub: 1, 
+    scrub: 1,
   },
-  y: "-300%", 
-  ease: Power1, 
+  y: "-300%",
+  ease: "Power4.easeInOut",
 });
 
+let sections = document.querySelectorAll(".images>img");
+sections.forEach((section) => {
+  gsap.to(".images img", {
+    scrollTrigger: {
+      trigger: "#featured_images",
+      start: "top top",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1,
+    },
+    y: "-300%",
+    ease: "Power4.easeInOut",
+  });
+});
